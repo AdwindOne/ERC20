83f471b82231d0eba4ee9784dee003b4d37920d0
Adwind <990198617@qq.com>
2018-04-11 16:39:54 +0800

    Update gasadd2.sol

--- gasadd2.sol
+++ gasadd2.sol
@@ -361,8 +361,3 @@ contract MyAdvancedToken is owned, token {
         msg.sender.transfer(amount * sellPrice);
     }
 }
-
-作者：yuyangray
-链接：https://www.jianshu.com/p/0d0dd2f09c99
-來源：简书
-著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
